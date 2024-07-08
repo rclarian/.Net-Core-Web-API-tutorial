822f342febe962a1fe89ca07be61f2c21d5853e9
Ryan Clarian <rclarian@gmail.com>
2024-07-08 15:38:22 +0800

    #3. Controller in Asp.net Core Web API - create controller
    #3. Controller in Asp.net Core Web API - create controller

---
+++ Web_API_Demo/Web_API_Demo/Controllers/ToDoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return null;
+        }
+    }
+}
