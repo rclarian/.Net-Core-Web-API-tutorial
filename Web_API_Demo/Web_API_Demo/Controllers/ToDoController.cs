96395f51102a549b2642c267745fd09d10458b58
Ryan Clarian <rclarian@gmail.com>
2024-07-08 15:57:00 +0800

    #4. Action method in Asp.net Core Web AP
    #4. Action method in Asp.net Core Web AP

--- Web_API_Demo/Web_API_Demo/Controllers/ToDoController.cs
+++ Web_API_Demo/Web_API_Demo/Controllers/ToDoController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Web_API_Demo.Models;
 
 namespace Web_API_Demo.Controllers
 {
@@ -7,10 +9,21 @@ namespace Web_API_Demo.Controllers
     [ApiController]
     public class ToDoController : ControllerBase
     {
+        private static List<ToDoItem> _toDoItems = new List<ToDoItem>
+        {
+            new ToDoItem { Id = 1, Title = "Task 1", Completed = false},
+            new ToDoItem { Id = 2, Title = "Task 2", Completed = true},
+            new ToDoItem { Id = 3, Title = "Task 3", Completed = false}
+        };
+
         [HttpGet]
         public IActionResult GetAll()
         {
-            return null;
+            if(_toDoItems.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(_toDoItems);
         }
     }
 }
---
+++ Web_API_Demo/Web_API_Demo/Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_API_Demo.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+    }
+}
